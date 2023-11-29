7e877b7fbed4ff33349b1e846d4f458cc063dae2
Yoshiki Ohira <tennmoku71@gmail.com>
2023-11-29 14:18:38 +0900

    add vad

--- voice_interaction.py
+++ voice_interaction.py
@@ -1,4 +1,5 @@
 from stt import google_stt
+from vad import google_vad
 import threading
 
 def callback_interim(user_utterance):
@@ -7,7 +8,14 @@ def callback_interim(user_utterance):
 def callback_final(user_utterance):
     print("final", user_utterance)
 
+def callback_vad(flag):
+    print("vad", flag)
+
 if __name__ == '__main__':
+
+    vad = google_vad.GOOGLE_WEBRTC()
+    vad_thread = threading.Thread(target=vad.vad_loop, args=(callback_vad, ))
+    vad_thread.start()
 
     stt_thread = threading.Thread(target=google_stt.main, args=(callback_interim, callback_final,))
     stt_thread.start()
